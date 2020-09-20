ee83d6afddbe00ce5dba826a6ef08e492e9b92ef
Marvin Menchu <mmenchu@outlook.com>
2020-09-19 18:00:26 -0600

    agregado modelo Pais

---
+++ app/Models/Pais.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pais extends Model
+{
+    protected $table = 'pais';
+    protected $primarykey = 'idPais';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'codigoPais',
+        'nombrePais'
+    ];
+    protected $guarded = [];
+}
