42be42b1b43a877e7a38e8132a97e114e523cae5
Marvin Menchu <mmenchu@outlook.com>
2020-11-07 01:02:16 -0600

    correcion de validacion en select

--- resources/views/contenido/diccionarioFormulario.blade.php
+++ resources/views/contenido/diccionarioFormulario.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.admin') @section('contenido')
+    @push('css')
+    <link rel="stylesheet" href="{{ asset('plugins/css/estilos.css') }}" />    
+    @endpush
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -39,6 +39,7 @@
     <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}" />
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet" />
+    @stack('css')
 </head>
 
 <body class="sidebar-mini layout-fixed sidebar-closed sidebar-collapse">
