04af7ac6d112e4f72faed764f3ba23b6baa97f20
Marvin Menchu <mmenchu@outlook.com>
2020-12-09 14:37:04 -0600

    optimizacion de query departamento

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Departamento;
-use App\Models\Pais;
 
 class DepartamentoController extends Controller
 {
     public function getDepartamento(Request $request) {
         if($request->ajax()){
-            $departamentos = Departamento::all();
+            $departamentos = Departamento::select('codigoDepartamento','nombreDepartamento')->get();
             return response()->json($departamentos);
         }
     }
