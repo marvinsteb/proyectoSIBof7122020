bab625a86ac95af8641ebcf2df725df000980b35
Marvin Menchu <mmenchu@outlook.com>
2021-01-01 17:59:56 -0600

    agregado span obligatorio para campos otros ingresos

--- resources/views/contenido/perfilEconomicoTransaccionalOtrosIngresos.blade.php
+++ resources/views/contenido/perfilEconomicoTransaccionalOtrosIngresos.blade.php
@@ -14,7 +14,7 @@
     <div class="row">
       <div class="col-sm-2">
         <div class="form-group">
-          <label for="tipoOtrosIngresosPet">Tipo de ingreso</label>
+          <label for="tipoOtrosIngresosPet">Tipo de ingreso <span>*</span></label>
           <select name="tipoOtrosIngresosPet" id="tipoOtrosIngresosPet1" class="form-control custom-select tipoOtrosIngresos select2 select2-hidden-accessible" style="width: 100%" required>
             <option value="" disabled="" selected="" data-select2-id="36">Selecciona</option>
             <option value="1" {{$doi->tipoOtrosIngresos == '1' ? 'selected' : ''}}>Actividades profecionales</option>
@@ -27,7 +27,7 @@
       </div>
       <div class="col-sm">
         <div class="form-group">
-          <label>Especificar otra fuente de ingresos</label>
+          <label>Especificar otra fuente de ingresos <span>*</span></label>
           <input name="DetalleOtrosIngresos1" id="DetalleOtrosIngresos1" type="text" class="form-control DetalleOtrosIngresos" placeholder="Especificar otra fuente de ingresos ..." maxlength="400" required value="{{$doi->detalleOtrosIngresos}}"> 
         </div>
       </div>
@@ -35,7 +35,7 @@
     <div class="row">
       <div class="col-sm-3">
         <div class="form-group">
-          <label for="moneda1">Moneda  {{$doi->tipoMoneda}}</label>
+          <label for="moneda1">Moneda <span>*</span></label>
           <select name="moneda" class="form-control custom-select moneda select2" style="width: 100%" required>
             <option value="" disabled selected>Selecciona</option>
               @foreach($monedas as $moneda)
@@ -50,7 +50,7 @@
       </div>
       <div class="col-sm-3">
         <div class="form-group">
-          <label>Monto aproximado ingresos</label>
+          <label>Monto aproximado ingresos <span>*</span></label>
           <input type="number" name="montoAproximado" class="form-control d-inline montoAproximado" placeholder="0.00" min="0" step=".01" style="text-align:right;" required value="{{$doi->montoAproximado}}">
         </div>
       </div>
